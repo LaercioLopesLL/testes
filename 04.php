7b6491817ed789b268d044a3fbb97f7eff0a110c
LaercioLopesLL <laerciolopesll@gmail.com>
2021-07-28 23:59:05 -0300

    4

--- 04.php
+++ 04.php
@@ -2,49 +2,34 @@
 
 
 function array_to_string($arr){
-	return "[ " . implode(", ", $arr) . " ]";	
+	echo "[ " . implode(", ", $arr) . " ]<br>";
 }
 
-function testaSequencia($original, $alterado, $indiceAtual, $maximoPulos, $removeAtual = false){
-	
-	if ((sizeof($original) - sizeof($alterado)) <= $maximoPulos){
-		if ($indiceAtual+1 == sizeof($alterado)){
-			echo "true 2";
-			return true;
+function testaSequencia($arr){
+	//imprime sequencia testada
+	//array_to_string($arr);
+	for ($i=0; $i < sizeof($arr); $i++) { 
+		if (isset($arr[$i+1]) && $arr[$i] >= $arr[$i+1]){
+			return false;
 		}
-		if (isset($alterado[$indiceAtual + 1])){
-			if ($alterado[$indiceAtual + 1] <= $alterado[$indiceAtual]){
-				echo array_to_string($alterado);
-				if ($removeAtual) {
-					array_splice($alterado, $indiceAtual, 1);	
-				} else {
-					array_splice($alterado, $indiceAtual + 1, 1);	
-				}
-				
-				echo array_to_string($alterado);
-				return testaSequencia($original, $alterado, $indiceAtual, $maximoPulos);
-			} 
-			return testaSequencia($original, $alterado, $indiceAtual+1, $maximoPulos);
-		}
-		echo "true 3";
-		return true;
-		
 	}
-
-	return false;
+	return true;
 }
+
+
 
 
 function sequenciaCrescente($arr, $maximoPulos = 1){
 	if (sizeof($arr) > 2){
-		$result = testaSequencia($arr, $arr, 0, $maximoPulos);
-		if ($result) return true;
-		return testaSequencia($arr, $arr, 0, $maximoPulos, true);
-
+		for ($i=0; $i < sizeof($arr); $i++) { 
+			$alterado = $arr;
+			array_splice($alterado, $i, 1);
+			if (testaSequencia($alterado)) return true;
+		}
+		return false;
 	}
-	echo "true 1";
 	return true;
 }
 
-$result = sequenciaCrescente([123, -17, -5, 1, 2, 3, 12, 43, 45]);
-var_dump($result);
+$resultado = sequenciaCrescente([3, 5, 67, 98, 3]);
+var_dump($resultado);
